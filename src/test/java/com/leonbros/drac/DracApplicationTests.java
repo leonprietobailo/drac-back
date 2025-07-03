4ad4e0250e983d0e5ad8ec9bbc400bce8b9d59da
Prieto Bailo, León Enrique <leprieto@indra.es>
2025-07-03 20:50:22 +0200

    spring boot initializer baseline

---
+++ src/test/java/com/leonbros/drac/DracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonbros.drac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
